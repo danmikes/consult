f2afe3503b58e86453852fc7f8dfd18acc8b0109
Daniel Mikes <max8@post.cz>
2025-10-10 14:56:18 +0200

    svelte

---
+++ README.md
@@ -0,0 +1,17 @@
+# Svelte-app
+- Mikes Consult
+- Npm Svelte
+
+## Code
+- [github](https://github.com/danmikes/svelte.git)
+
+## Install
+- `npm install`
+
+## Develop
+- `npm run dev`
+- `git push`
+
+## View
+- [local](http://localhost:5173)
+- [remote](https://danmikes.github.io/svelte)
---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Head from './component/Head.svelte';
+	import Foot from './component/Foot.svelte';
+	import Home from './page/Home.svelte';
+	import Blog from './page/Blog.svelte';
+	import Contact from './page/Contact.svelte';
+
+	import { onMount } from 'svelte';
+
+	let currentPage = 'home';
+	
+	onMount(() => {
+		currentPage = window.location.hash.slice(1) || 'home';
+		
+		window.addEventListener('hashchange', () => {
+			currentPage = window.location.hash.slice(1) || 'home';
+		});
+	});
+</script>
+
+<Head />
+<main>
+	{#if currentPage === 'home'}
+		<Home />
+	{:else if currentPage === 'blog'}
+		<Blog />
+	{:else if currentPage === 'contact'}
+		<Contact />
+	{/if}
+</main>
+<Foot />
