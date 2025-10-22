bbfcba5dc77d1011ec1050c3f63430bdf8b75cf8
Daniel Mikes <max8@post.cz>
2025-10-22 15:10:42 +0200

    Revert "build-time"
    This reverts commit 08ed953988379f8676da1216c82906114ec5dda2.

--- inject_build.js
+++
@@ -1,4 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-const buildTime = new Date().toLocaleString();
-const html = readFileSync('index.html', 'utf8').replace(/__BUILD_TIME__/g, buildTime);
-writeFileSync('index.html', html);
